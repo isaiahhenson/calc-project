2d496e6bcc965fd2802f815d0529c9fce5e96499
Isaiah Henson <Devpuddles@gmail.com>
2025-01-30 18:37:27 -0500

    added event listeners and text content

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('hello world');
+let firstVal;
+let operation;
+let secondVal;
+
+function add(firstVal, secondVal) {
+  return firstVal + secondVal;
+}
+function subtract(firstVal, secondVal) {
+  return firstVal - secondVal;
+}
+function multiply(firstVal, secondVal) {
+  return firstVal * secondVal;
+}
+function divide(firstVal, secondVal) {
+  return firstVal / secondVal;
+}
+
+function operate(num1, op, num2) {
+  if (op == '+') {
+    return add(num1, num2);
+  }
+  if (op == '-') {
+    return subtract(num1, num2);
+  }
+  if (op == '*') {
+    return multiply(num1, num2);
+  }
+  if (op == '/') {
+    return divide(num1, num2);
+  }
+}
+
+console.log(operate(8, '/', 2));
+
+let screen = document.getElementById('top-div');
+
+let one = document.getElementById('1');
+one.addEventListener('click', () => (screen.textContent += one.textContent));
