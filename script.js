97928305ec0fc6b53dcd85bfb81d06c1d6b3eb52
Isaiah Henson <Devpuddles@gmail.com>
2025-01-31 16:23:23 -0500

    more logic to the project

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-let firstVal;
-let operation;
-let secondVal;
+let storedVal = '';
 
 function add(firstVal, secondVal) {
   return firstVal + secondVal;
@@ -30,9 +28,160 @@ function operate(num1, op, num2) {
   }
 }
 
-console.log(operate(8, '/', 2));
+//store screen values into three variables
+//upon click run operate function(w/storedvalues) thats firstVal, secondVal, and operation.
+
+function splitSort(str) {
+  let split = str.split('');
+
+  console.log(split);
+
+  if (
+    split.includes('+') ||
+    split.includes('-') ||
+    split.includes('/') ||
+    split.includes('*')
+  ) {
+    for (let i = 0; i < split.length; i++) {
+      console.log(split[i]);
+
+      if (split[i] === '+' || split[i] === '-' || split[i] === '*') {
+        let index = split.indexOf(split[i]);
+        let firstVal = split.slice(0, index);
+        let operation = split[i];
+        let secondVal = split.slice(index + 1, split.length);
+
+        console.log(split.indexOf(split[i]));
+        return {
+          index: index,
+          firstVal: firstVal,
+          operation: operation,
+          secondVal: secondVal,
+        };
+      }
+    }
+  }
+}
+
+let values = splitSort(storedVal);
+console.log(values);
 
 let screen = document.getElementById('top-div');
 
 let one = document.getElementById('1');
-one.addEventListener('click', () => (screen.textContent += one.textContent));
+one.addEventListener('click', () => {
+  screen.textContent += one.textContent;
+  storedVal += one.textContent;
+  console.log(storedVal);
+});
+let two = document.getElementById('2');
+two.addEventListener('click', () => {
+  screen.textContent += two.textContent;
+  storedVal += two.textContent;
+  console.log(storedVal);
+});
+let three = document.getElementById('3');
+three.addEventListener('click', () => {
+  screen.textContent += three.textContent;
+  storedVal += three.textContent;
+  console.log(storedVal);
+});
+let four = document.getElementById('4');
+four.addEventListener('click', () => {
+  screen.textContent += four.textContent;
+  storedVal += four.textContent;
+  console.log(storedVal);
+});
+let five = document.getElementById('5');
+five.addEventListener('click', () => {
+  screen.textContent += five.textContent;
+  storedVal += five.textContent;
+  console.log(storedVal);
+});
+let six = document.getElementById('6');
+six.addEventListener('click', () => {
+  screen.textContent += six.textContent;
+  storedVal += six.textContent;
+  console.log(storedVal);
+});
+let seven = document.getElementById('7');
+seven.addEventListener('click', () => {
+  screen.textContent += seven.textContent;
+  storedVal += seven.textContent;
+  console.log(storedVal);
+});
+let eight = document.getElementById('8');
+eight.addEventListener('click', () => {
+  screen.textContent += eight.textContent;
+  storedVal += eight.textContent;
+  console.log(storedVal);
+});
+let nine = document.getElementById('9');
+nine.addEventListener('click', () => {
+  screen.textContent += nine.textContent;
+  storedVal += nine.textContent;
+  console.log(storedVal);
+});
+
+let zero = document.getElementById('10');
+zero.addEventListener('click', () => {
+  screen.textContent += zero.textContent;
+  storedVal += zero.textContent;
+  console.log(storedVal);
+});
+
+let eleven = document.getElementById('11');
+eleven.addEventListener('click', () => {
+  screen.textContent += eleven.textContent;
+  storedVal += eleven.textContent;
+  console.log(storedVal);
+});
+
+let twelve = document.getElementById('12');
+twelve.addEventListener('click', () => {
+  screen.textContent += twelve.textContent;
+  storedVal += twelve.textContent;
+  console.log(storedVal);
+});
+
+let thirteen = document.getElementById('13');
+thirteen.addEventListener('click', () => {
+  screen.textContent += thirteen.textContent;
+  storedVal += thirteen.textContent;
+  console.log(storedVal);
+});
+
+let fourteen = document.getElementById('14');
+fourteen.addEventListener('click', () => {
+  screen.textContent += fourteen.textContent;
+  storedVal += fourteen.textContent;
+  console.log(storedVal);
+});
+
+let fifteen = document.getElementById('15');
+fifteen.addEventListener('click', () => {
+  screen.textContent += fifteen.textContent;
+  console.log(storedVal);
+
+  if (screen.textContent.length > 3) {
+    console.log(splitSort(storedVal));
+  }
+});
+
+let sixteen = document.getElementById('16');
+sixteen.addEventListener('click', () => {
+  console.log(storedVal);
+});
+
+let seventeen = document.getElementById('17');
+seventeen.addEventListener('click', () => {
+  console.log(storedVal);
+});
+
+// let firstVal = values.firstVal;
+// let operation = values.operation;
+// let secondVal = values.secondVal;
+
+// console.log(firstVal);
+// console.log(operation);
+// console.log(secondVal);
